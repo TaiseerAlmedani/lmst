2a8a77508d4d76e678b8705f2aef5591453481fe
Essam Dalal <essamdalal7@gmail.com>
2021-06-25 22:56:36 +0300

    add data in show course

--- app/Http/Controllers/PageCourseController.php
+++ app/Http/Controllers/PageCourseController.php
@@ -13,6 +13,15 @@ class PageCourseController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+
+
+    }
+
+
     public function index()
     {
 
