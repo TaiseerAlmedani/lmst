3b1706e0027f07fc98e2790066ad1b4d550d8568
Essam Dalal <essamdalal7@gmail.com>
2021-06-28 23:27:22 +0300

    fix carousel in course page

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,3 +1,41 @@
+<style>
+    .col-carousel {
+        margin: 70px 0;
+    }
+
+    /* owl nav */
+    .owl-prev span,
+    .owl-next span {
+        color: #FFF;
+    }
+
+    .owl-prev span:hover,
+    .owl-next span:hover {
+        color: #8199A3;
+    }
+
+    .owl-prev,
+    .owl-next {
+        position: absolute;
+        top: 0;
+        height: 100%;
+    }
+
+    .owl-prev {
+        left: 7px;
+    }
+
+    .owl-next {
+        right: 7px;
+    }
+
+    /* removing blue outline from buttons */
+    button:focus,
+    button:active {
+        outline: none;
+    }
+
+</style>
 <x-layouts.app>
     <section class="hero is-info is-fullheight">
         <div class="hero-head">
@@ -18,60 +56,68 @@
             </div>
         </div>
     </section>
+
     <section class="section">
         <div class="continer m-6">
+
             @foreach ($categories as $category)
                 <p class="is-size-3">{{ $category->slug }}</p>
-                <div class="owl-carousel">
-                @foreach ($category->courses as $course)
+                <div class="row align-items-center">
+                    <div class="col-12 col-carousel">
+                        <div class="owl-carousel carousel-main">
 
-                        <div>
-                            <div class="card is-shady">
-                                <div class="card-image">
-                                    <figure class="image is-4by3">
-                                        <img src="{{ $course->course_img }}" alt="butterfly image"
-                                            class="modal-button" data-target="modal-image4">
-                                    </figure>
-                                </div>
-                                <div class="card-content  modal-button" data-target="modal-card2">
-                                    <div class="content">
-                                        <h4>{{ Str::limit($course->slug , 20, '...')}}</h4>
-                                        <p>{{ Str::limit($course->description , 50, '...')}}</p>
-                                        <div class="max-2">
-                                            <div class="star_icon pl-1 ">
-                                                @if ( $course->rate === 1 )
-                                                    <i class="mdi mdi-star"></i>
-                                                @elseif( $course->rate === 2 )
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                @elseif( $course->rate === 3 )
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                @elseif( $course->rate === 4 )
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                    <i class="mdi mdi-star"></i>
-                                                @else
-                                                <i class="mdi mdi-star"></i>
-                                                <i class="mdi mdi-star"></i>
-                                                <i class="mdi mdi-star"></i>
-                                                <i class="mdi mdi-star"></i>
-                                                <i class="mdi mdi-star"></i>
-                                                @endif
+                            @foreach ($category->courses as $course)
+
+                                <div>
+                                    <div class="card is-shady">
+                                        <div class="card-image">
+                                            <figure class="image is-4by3">
+                                                <img src="{{ $course->course_img }}" alt="butterfly image"
+                                                    class="modal-button" data-target="modal-image4">
+                                            </figure>
+                                        </div>
+                                        <div class="card-content  modal-button" data-target="modal-card2">
+                                            <div class="content">
+                                                <h4>{{ Str::limit($course->slug, 20, '...') }}</h4>
+                                                <p>{{ Str::limit($course->description, 50, '...') }}</p>
+                                                <div class="max-2">
+                                                    <div class="star_icon pl-1 ">
+                                                        @if ($course->rate === 1)
+                                                            <i class="mdi mdi-star"></i>
+                                                        @elseif( $course->rate === 2 )
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                        @elseif( $course->rate === 3 )
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                        @elseif( $course->rate === 4 )
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                        @else
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                            <i class="mdi mdi-star"></i>
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                                <a href="{{ route('course.show', $course) }}">
+                                                    <span class="button is-link modal-button is-fullwidth">View</span>
+                                                </a>
                                             </div>
                                         </div>
-                                        <a href="{{ route('course.show', $course) }}">
-                                            <span class="button is-link modal-button is-fullwidth">View</span>
-                                        </a>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
 
-                @endforeach
-            </div>
+                            @endforeach
+
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </div>
     </section>
@@ -81,19 +127,23 @@
 <script>
     $(document).ready(function() {
 
-        $(".owl-carousel").owlCarousel({
+        $('.carousel-main').owlCarousel({
+            items: 3,
             loop: true,
-            // center: true,
-            nav: false,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            margin: 10,
+            nav: true,
+            dots: false,
             responsive: {
                 600: {
                     items: 4
                 },
 
             },
-            dots: false,
-            margin: 10,
+            navText: ['<span class="fa fa-angle-left fa-2x"></span>',
+                '<span class="fa fa-angle-right fa-2x"></span>'
+            ],
         })
     })
-
 </script>
